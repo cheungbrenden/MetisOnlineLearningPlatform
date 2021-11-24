2a70043c86fca1e9495c81bc827e12c35a490cf9
Victor Wen <vicwen02@g.ucla.edu>
2021-11-23 22:09:03 -0800

    Finished basic dynamic functions for assignment question

--- client/src/AssignmentQuestion.js
+++ client/src/AssignmentQuestion.js
@@ -1,7 +1,36 @@
 import NavWindow from "./Navbars";
 import './AssignmentQuestion.css'
+import { useState } from "react";
+
+function ResultBox(props) {
+    if (props.correct) {
+        return (
+            <div id="correctResultBox" className="resultBox">
+                Great job! <br/>
+                <b className="font-oblique">y</b> = 3,
+                <b className="font-oblique"> u</b> = 4 <br/>
+                Move on to <b>Problem 2</b> (SLO 3.1)
+            </div>
+        );
+    }
+    else {
+        return (
+            <div id="incorrectResultBox" className="resultBox">
+                Incorrect answer. Please try again. <br/> <br/>
+                Try solving for <b className="font-oblique">u</b> first. 
+                Then solve for <b className="font-oblique">y</b>.
+            </div>
+        );
+    }
+}
 
 function AssignmentQuestion() {
+
+    const [first, setFirst] = useState('');
+    const [second, setSecond] = useState('');
+    const [showHint, setShowHint] = useState(false);
+    const [showResult, setShowResult] = useState(false);
+
     return (
         <NavWindow>
             <div id="assignmentQuestion">
@@ -23,14 +52,9 @@ function AssignmentQuestion() {
                         right: "15px"
                     }}>2 points</span>
                 </div>
-                <div className="infoBox" style={{
+                <div id="questionInfo" className="infoBox" style={{
                     gridColumnStart: "1",
                     gridColumnEnd: "3",
-                    padding: "20px",
-                    fontSize: "36px",
-                    display: "grid",
-                    gridTemplateColumns: "1fr 1fr",
-                    gridTemplateRows: "1fr 1fr",
                 }}>
                     <div style={{
                         gridColumn: 1,
@@ -44,28 +68,53 @@ function AssignmentQuestion() {
                             <div>u + 2y = 10</div>
                         </div>
                     </div>
-                    <button className="font-darkBlue font-bold" style={{
-                        height: "50px",
-                        width: "150px",
-                        fontSize: "15px",
-                        backgroundColor: "white",
-                        borderRadius: "35px",
-                        border: "1px solide #3823a8"
-                    }}>
-                        Need a hint?
-                    </button>
-                    <form>
-                        Input Answer:
-                        <div className="answer">u= <input className="answerBox" type="text"/></div>
-                        <div className="answer">y= <input className="answerBox" type="text"/></div>
-                        <input style={{
-                            color: "#3823a8",
-                            backgroundColor: "#f5c24c",
-                            borderRadius: "35px",
-                            width: "200px",
-                            border: "1px solide #3823a8"
-                        }} type="submit" />
-                    </form>
+                    <div>
+                        <button id="hintButton" className="font-darkBlue font-bold" onClick={() => setShowHint(true)}>
+                            Need a hint?
+                        </button>
+                        { showHint && (
+                            <div className="hintBox">
+                                <button id="exitHintButton" onClick={() => setShowHint(false)}>X</button>
+                                Hint 1 of 1 <br/> <br/>
+                                Use one equation to solve for <b className="font-oblique">u</b>.
+                                Then substitute in the other equation to solve for <b className="font-oblique">y</b>.
+                            </div>
+                        )}
+                    </div>
+                    <div>
+                        <form>
+                            Input Answer:
+                            <label className="answer">
+                                u= <input className="answerBox" 
+                                    type="text"
+                                    name="u" 
+                                    value={first} 
+                                    autoComplete={false}
+                                    onChange={(event) => {
+                                        setFirst(event.target.value);
+                                        setShowResult(false);
+                                    }}
+                                    />
+                            </label>
+                            <label className="answer">
+                                y= <input className="answerBox" 
+                                    type="text"
+                                    name="y"
+                                    value={second}
+                                    autoComplete={false}
+                                    onChange={(event) => {
+                                        setSecond(event.target.value);
+                                        setShowResult(false);
+                                    }}/>
+                            </label>
+                        </form>
+                        <button id="submitButton" type="submit" onClick={function() {
+                            setShowResult(true);
+                        }}>
+                            Submit
+                        </button>
+                    </div>
+                    { showResult && <ResultBox correct={first=="4" && second=="3"}/> }
                 </div>
             </div>
         </NavWindow>
